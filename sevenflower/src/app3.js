2c5150cd407ef60f70914f77f388788e07b68bcf
werbug <857091291@qq.com>
2017-01-09 21:52:36 +0800

    2

--- sevenflower/src/app3.js
+++
@@ -1,8 +0,0 @@
-require('./styles/usage/app.scss');
-
-var common = require('./scripts/utils/util.common.js');
-var html = require('./scripts/tpls/details.html');
-
-common.render(html);
-
-require('./scripts/views/details.js');
